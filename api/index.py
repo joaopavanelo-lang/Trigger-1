c6c7d0b6ca385277dd78575e0151aa4251b8165a
joaopavanelo-lang <joao.pavanelo@shopee.com>
2026-03-12 19:18:56 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -13,14 +13,14 @@ HEADERS = {
 
 # Lista de workflows a serem acionados (Ajustado para 'workflow' no singular para o loop funcionar)
 WORKFLOWS = [
-    {"repo": "QueueListLog", "workflow": "queuelistsp5.yaml"},
-    {"repo": "BasePending", "workflow": "main_expedicao.yaml"},
-    {"repo": "Base_Handedover", "workflow": "main_expedicao.yaml"},
-    {"repo": "Base_3PL", "workflow": "main_expedicao.yaml"},
-    {"repo": "Base_Packed_FerramentaFIFO", "workflow" : "main_base_to_packed.yaml"},
-    {"repo": "Queuelist", "workflow" : "queue_list_sp5.yml"},
-    {"repo": "base_inbound", "workflow" : "main_inbound.yaml"},
-    {"repo": "Base-ended", "workflow" : "main_ended.yaml"},
+    # {"repo": "QueueListLog", "workflow": "queuelistsp5.yaml"},
+    # {"repo": "BasePending", "workflow": "main_expedicao.yaml"},
+    # {"repo": "Base_Handedover", "workflow": "main_expedicao.yaml"},
+    # {"repo": "Base_3PL", "workflow": "main_expedicao.yaml"},
+    # {"repo": "Base_Packed_FerramentaFIFO", "workflow" : "main_base_to_packed.yaml"},
+    # {"repo": "Queuelist", "workflow" : "queue_list_sp5.yml"},
+    # {"repo": "base_inbound", "workflow" : "main_inbound.yaml"},
+    # {"repo": "Base-ended", "workflow" : "main_ended.yaml"},
 ]
 
 # Rota principal para verificar se o app está no ar
